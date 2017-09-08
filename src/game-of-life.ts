3885ea981615a516bad8a243083d1d86f112fc60
Barend Erasmus <developersworkspace@gmail.com>
2017-09-08 09:22:38 +0200

    update

--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -40,28 +40,28 @@ export function getNumberOfNeighbours(x: number, y: number, grid: number[][]): n
     let neighbours: number = 0;
 
     if (x > 0 && y > 0 && grid[y - 1][x - 1]) {
-        neighbours ++;
+        neighbours++;
     }
     if (x > 0 && grid[y][x - 1]) {
         ++neighbours;
     }
     if (x > 0 && y < grid.length - 1 && grid[y + 1][x - 1]) {
-        neighbours ++;
+        neighbours++;
     }
     if (y > 0 && grid[y - 1][x]) {
-        neighbours ++;
+        neighbours++;
     }
     if (y < grid.length - 1 && grid[y + 1][x]) {
-        neighbours ++;
+        neighbours++;
     }
     if (y > 0 && x < grid[0].length - 1 && grid[y - 1][x + 1]) {
-        neighbours ++;
+        neighbours++;
     }
     if (x < grid[0].length - 1 && grid[y][x + 1]) {
-        neighbours ++;
+        neighbours++;
     }
     if (x < grid[0].length - 1 && y < grid.length - 1 && grid[y + 1][x + 1]) {
-        neighbours ++;
+        neighbours++;
     }
     return neighbours;
 }
@@ -71,15 +71,15 @@ export function display(grid: number[][]): void {
     let numberOfCellsAlive: number = 0;
 
     const rowStrings: string[] = [];
-    
+
     for (const row of grid) {
         const columnStrings: string[] = [];
 
         for (const column of row) {
             if (column === 1) {
-                numberOfCellsAlive ++;
+                numberOfCellsAlive++;
                 columnStrings.push(`<td class="alive"></td>`);
-            }else {
+            } else {
                 columnStrings.push(`<td class="dead"></td>`);
             }
         }
@@ -102,13 +102,12 @@ export let interval = null;
 export let grounds: number[][] = null;
 
 export function initialize() {
-    
+
     document.getElementById("btn-start").removeAttribute('disabled');
-    document.getElementById("btn-reinitialize").removeAttribute('disabled');
+    document.getElementById("btn-load-pattern").removeAttribute('disabled');
     document.getElementById("btn-pause").setAttribute('disabled', 'disabled');
 
     grounds = makeArray(75, 75, 0);
-
 
     for (let y = 0; y < grounds.length; y++)
         for (let x = 0; x < grounds[0].length; x++) {
@@ -121,9 +120,8 @@ export function initialize() {
 export function start() {
 
     document.getElementById("btn-pause").removeAttribute('disabled');
-    document.getElementById("btn-reinitialize").setAttribute('disabled', 'disabled');
+    document.getElementById("btn-load-pattern").setAttribute('disabled', 'disabled');
     document.getElementById("btn-start").setAttribute('disabled', 'disabled');
-
 
     interval = setInterval(() => {
         grounds = processMatrix(grounds);
@@ -135,9 +133,60 @@ export function start() {
 export function pause() {
 
     document.getElementById("btn-start").removeAttribute('disabled');
-    document.getElementById("btn-reinitialize").removeAttribute('disabled');
+    document.getElementById("btn-load-pattern").removeAttribute('disabled');
     document.getElementById("btn-pause").setAttribute('disabled', 'disabled');
 
-
     clearInterval(interval);
+}
+export function selectPattern() {
+
+    const name: string = (<any>document.getElementById("select-pattern")).value;
+
+    const patterns: {} = {
+        blinker: [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ],
+        beacon: [
+            [0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0],
+        ],
+        glider: [
+            [0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 0, 1, 1, 0],
+            [0, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+        ],
+    };
+
+    if (name === 'random') {
+        for (let y = 0; y < grounds.length; y++)
+            for (let x = 0; x < grounds[0].length; x++) {
+                grounds[y][x] = Math.random() > 0.5 ? 1 : 0;
+            }
+    } else {
+
+        const pattern: number[][] = patterns[name];
+
+        grounds = makeArray(75, 75, 0);
+
+        const yOffset: number = Math.floor((75 - pattern.length) / 2);
+        const xOffset: number = Math.floor((75 - pattern[0].length) / 2);
+
+        for (let y = 0; y < pattern.length; y++) {
+            for (let x = 0; x < pattern[y].length; x++) {
+                grounds[y + yOffset][x + xOffset] = pattern[y][x];
+            }
+        }
+    }
+
+    display(grounds);
 }
